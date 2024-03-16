bb305be15d2282323cbfb20ae0f1026aa9371f85
PRAKHAR-A-TRIPATHI <151377167+PrakharATripathi@users.noreply.github.com>
2024-03-16 19:01:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,13 @@ connectDB();
 // middleware for
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
 app.use("/user", router);
 app.use("/task",taskRoute); 
 
