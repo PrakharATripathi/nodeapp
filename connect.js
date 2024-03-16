c8edee3a50ad72b34a8bfc3dfe293fb602647820
PRAKHAR-A-TRIPATHI <151377167+PrakharATripathi@users.noreply.github.com>
2024-03-16 18:15:24 +0530

    update

--- connect.js
+++ connect.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const connectDB = () => {
-    mongoose.connect(process.env.MONGO_URL, { dbName: "BackendApi", })
-        .then((c) => {
-            console.log(`Connected to MongoDB: ${c.connection.host}`);
-        })
-        .catch((err) => {
-            console.error(`Error connecting to MongoDB: ${err}`);
-        });
+ const connectDB = () => {
+    console.log(process.env.MONGO_URL)
+  mongoose
+    .connect(process.env.MONGO_URL, {
+      dbName: "backendapi",
+    })
+    .then((c) => console.log(`Database Connected with ${c.connection.host}`))
+    .catch((e) => console.log(e));
 };
-
+  
 module.exports = connectDB;
